1a4379c9097f9a2ca71afabf241f0e3781828ecd
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-01-08 23:36:29 +0100

    Add request param on index action

--- src/Listabierta/Bundle/MunicipalesBundle/Controller/DefaultController.php
+++ src/Listabierta/Bundle/MunicipalesBundle/Controller/DefaultController.php
@@ -3,10 +3,11 @@
 namespace Listabierta\Bundle\MunicipalesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         return $this->render('MunicipalesBundle:Default:index.html.twig');
     }
