8866a8ea42be6c284ea81def2c5dd49bdce7beae
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-01-09 01:52:53 +0100

    Implement candidacy step 1 type form

---
+++ src/Listabierta/Bundle/MunicipalesBundle/Form/Type/CandidacyStep1Type.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Listabierta\Bundle\MunicipalesBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CandidacyStep1Type extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', 'text', array('required' => true))
+        	    ->add('lastname', 'text', array('required' => true))
+        	    ->add('dni', 'text', array('required' => true))
+        	    ->add('email', 'email', array('required' => true))
+        	    ->add('province', 'text', array('required' => true))
+        	    ->add('town', 'text', array('required' => true))
+        	    ->add('phone', 'text', array('required' => true))
+	            ->add('continue', 'submit', array('attr' => array('class' => 'submit')));
+    }
+	
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+    }
+
+    public function getName()
+    {
+        return 'candidacy_step1';
+    }
+}
