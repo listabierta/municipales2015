1af7a003b90672ecdcd4809a7ce0b6de7c2e1106
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-01-09 17:33:20 +0100

    Add assert length to form type 1

--- src/Listabierta/Bundle/MunicipalesBundle/Form/Type/CandidacyStep1Type.php
+++ src/Listabierta/Bundle/MunicipalesBundle/Form/Type/CandidacyStep1Type.php
@@ -15,11 +15,23 @@ class CandidacyStep1Type extends AbstractType
         				'required' => true, 
         				'constraints' => array(
         					new Assert\NotBlank(),
+        					new Assert\Length(array(
+        							'min'        => 2,
+        							'max'        => 255,
+        							'minMessage' => 'Your first name must be at least {{ limit }} characters long',
+        							'maxMessage' => 'Your first name cannot be longer than {{ limit }} characters long',
+        					))
         				)))
         	    ->add('lastname', 'text', array(
         	    		'required' => true, 
         				'constraints' => array(
         					new Assert\NotBlank(),
+        					new Assert\Length(array(
+        							'min'        => 2,
+        							'max'        => 255,
+        							'minMessage' => 'Your lastname must be at least {{ limit }} characters long',
+        							'maxMessage' => 'Your lastname cannot be longer than {{ limit }} characters long',
+        					))
         				)))
         	    ->add('dni', 'text', array(
         	    		'required' => true, 
@@ -36,16 +48,34 @@ class CandidacyStep1Type extends AbstractType
         	    		'required' => true, 
         				'constraints' => array(
         					new Assert\NotBlank(),
+        					new Assert\Length(array(
+        						'min'        => 2,
+        						'max'        => 255,
+        						'minMessage' => 'Your province must be at least {{ limit }} characters long',
+        						'maxMessage' => 'Your province cannot be longer than {{ limit }} characters long',
+        					))
         				)))
         	    ->add('town', 'text', array(
         	    		'required' => true, 
         				'constraints' => array(
         					new Assert\NotBlank(),
+        					new Assert\Length(array(
+        							'min'        => 2,
+        							'max'        => 255,
+        							'minMessage' => 'Your town must be at least {{ limit }} characters long',
+        							'maxMessage' => 'Your town cannot be longer than {{ limit }} characters long',
+        					))
         				)))
         	    ->add('phone', 'text', array(
         	    		'required' => true, 
         				'constraints' => array(
         					new Assert\NotBlank(),
+        					new Assert\Length(array(
+        						'min'        => 9,
+        						'max'        => 12,
+        						'minMessage' => 'Your phone must be at least {{ limit }} characters long',
+        						'maxMessage' => 'Your phone cannot be longer than {{ limit }} characters long',
+        						))
         				)))
 	            ->add('continue', 'submit', array('attr' => array('class' => 'submit')));
     }
