00b1fbb839d7ac17f8f9195eecba636986a01413
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-01-09 17:14:12 +0100

    Add contrainst to candidacy step 1 type form

--- src/Listabierta/Bundle/MunicipalesBundle/Form/Type/CandidacyStep1Type.php
+++ src/Listabierta/Bundle/MunicipalesBundle/Form/Type/CandidacyStep1Type.php
@@ -5,18 +5,48 @@ namespace Listabierta\Bundle\MunicipalesBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class CandidacyStep1Type extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name', 'text', array('required' => true))
-        	    ->add('lastname', 'text', array('required' => true))
-        	    ->add('dni', 'text', array('required' => true))
-        	    ->add('email', 'email', array('required' => true))
-        	    ->add('province', 'text', array('required' => true))
-        	    ->add('town', 'text', array('required' => true))
-        	    ->add('phone', 'text', array('required' => true))
+        $builder->add('name', 'text', array(
+        				'required' => true, 
+        				'constraints' => array(
+        					new Assert\NotBlank(),
+        				)))
+        	    ->add('lastname', 'text', array(
+        	    		'required' => true, 
+        				'constraints' => array(
+        					new Assert\NotBlank(),
+        				)))
+        	    ->add('dni', 'text', array(
+        	    		'required' => true, 
+        				'constraints' => array(
+        					new Assert\NotBlank(),
+        				)))
+        	    ->add('email', 'email', array(
+        	    		'required' => true, 
+        				'constraints' => array(
+        					new Assert\NotBlank(),
+        					new Assert\Email(),
+        				)))
+        	    ->add('province', 'text', array(
+        	    		'required' => true, 
+        				'constraints' => array(
+        					new Assert\NotBlank(),
+        				)))
+        	    ->add('town', 'text', array(
+        	    		'required' => true, 
+        				'constraints' => array(
+        					new Assert\NotBlank(),
+        				)))
+        	    ->add('phone', 'text', array(
+        	    		'required' => true, 
+        				'constraints' => array(
+        					new Assert\NotBlank(),
+        				)))
 	            ->add('continue', 'submit', array('attr' => array('class' => 'submit')));
     }
 	
