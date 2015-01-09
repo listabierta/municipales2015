31f031d372991577945cc6d36b2d507f7b269d2b
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-01-09 01:54:34 +0100

    Implement a stub for candidacy controller step 1

--- src/Listabierta/Bundle/MunicipalesBundle/Controller/CandidacyController.php
+++ src/Listabierta/Bundle/MunicipalesBundle/Controller/CandidacyController.php
@@ -5,6 +5,8 @@ namespace Listabierta\Bundle\MunicipalesBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use Listabierta\Bundle\MunicipalesBundle\Form\Type\CandidacyStep1Type;
+
 class CandidacyController extends Controller
 {
     public function indexAction(Request $request)
@@ -14,6 +16,15 @@ class CandidacyController extends Controller
     
     public function step1Action(Request $request)
     {
-    	return $this->render('MunicipalesBundle:Candidacy:step1.html.twig');
+    	$form = $this->createForm(new CandidacyStep1Type());
+    	
+    	$form->handleRequest($request);
+    	
+    	if ($form->isValid())
+    	{
+    		
+    	}
+    	
+    	return $this->render('MunicipalesBundle:Candidacy:step1.html.twig', array('form' => $form->createView()));
     }
 }
