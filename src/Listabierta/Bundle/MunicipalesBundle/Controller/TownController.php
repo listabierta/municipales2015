6f2311693b21136609e316d5ee755f5e6af86a77
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-01-09 04:08:10 +0100

    Add Town controller

---
+++ src/Listabierta/Bundle/MunicipalesBundle/Controller/TownController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Listabierta\Bundle\MunicipalesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Listabierta\Bundle\MunicipalesBundle\Form\Type\TownStep1Type;
+
+class TownController extends Controller
+{
+	public function indexAction(Request $request = NULL)
+	{
+		$this->step1Action($request);
+	}
+	
+	public function vote1Action($town = NULL, Request $request = NULL)
+	{
+		$form = $this->createForm(new TownStep1Type(), NULL, array(
+				'action' => $this->generateUrl('municipales_candidacy_step1'),
+				'method' => 'POST',
+		)
+		);
+		 
+		$form->handleRequest($request);
+		
+		if ($form->isValid())
+		{
+
+		}
+
+		return $this->render('MunicipalesBundle:Town:step1.html.twig', array(
+				'town' => $town, 
+				'form' => $form->createView(),
+				'errors' => $form->getErrors()
+		));
+	}
+}
