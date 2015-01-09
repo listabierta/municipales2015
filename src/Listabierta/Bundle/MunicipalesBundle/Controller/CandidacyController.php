0d13dbfdf3ab32a50b1c365bbd213051422b92d2
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-01-09 02:17:14 +0100

    Implement form for step 2

--- src/Listabierta/Bundle/MunicipalesBundle/Controller/CandidacyController.php
+++ src/Listabierta/Bundle/MunicipalesBundle/Controller/CandidacyController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 use Listabierta\Bundle\MunicipalesBundle\Form\Type\CandidacyStep1Type;
+use Listabierta\Bundle\MunicipalesBundle\Form\Type\CandidacyStep2Type;
 
 class CandidacyController extends Controller
 {
@@ -16,15 +17,36 @@ class CandidacyController extends Controller
     
     public function step1Action(Request $request)
     {
-    	$form = $this->createForm(new CandidacyStep1Type());
+    	$form = $this->createForm(new CandidacyStep1Type(), NULL, array(
+    			'action' => $this->generateUrl('municipales_candidacy_step1'),
+			    'method' => 'POST',
+    			)
+    	);
     	
     	$form->handleRequest($request);
-    	
+
     	if ($form->isValid())
     	{
+    		$warnings = array();
     		
+    		$form2 = $this->createForm(new CandidacyStep2Type(), NULL, array(
+    				'action' => $this->generateUrl('municipales_candidacy_step2'),
+    				'method' => 'POST',
+    		));
+    		
+    		$form2->handleRequest($request);
+    		
+    		return $this->render('MunicipalesBundle:Candidacy:step2.html.twig', array(
+    				'warnings' => $warnings,
+    				'errors' => $form->getErrors(),
+    				'form' => $form2->createView()
+    			)
+    		);
     	}
     	
-    	return $this->render('MunicipalesBundle:Candidacy:step1.html.twig', array('form' => $form->createView()));
+    	return $this->render('MunicipalesBundle:Candidacy:step1.html.twig', array(
+    			'form' => $form->createView(),
+    			'errors' => $form->getErrors(),
+    	));
     }
 }
