288faba260f164513ca9a58f185513428aca6b54
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-01-11 06:18:42 +0100

    Implement more the stub for verify a mobile number in sms inbound

--- src/Listabierta/Bundle/MunicipalesBundle/Controller/SMSInboundController.php
+++ src/Listabierta/Bundle/MunicipalesBundle/Controller/SMSInboundController.php
@@ -5,6 +5,7 @@ namespace Listabierta\Bundle\MunicipalesBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Listabierta\Bundle\MunicipalesBundle\Entity\PhoneVerified;
 
 class SMSInboundController extends Controller
 {
@@ -13,20 +14,39 @@ class SMSInboundController extends Controller
 		$query = $request->query;
 		//$msisdn = $query->msisdn;
 
-		$phone = 1234;
+		$phone = '555443322';
+		$email = $this->container->getParameter('admin_email');
 		
-		$message = \Swift_Message::newInstance()
-		->setSubject('Telefono movil ' . $phone . ' verificado correctamenet')
-		->setFrom('verificaciones@municipales2015.listabierta.org')
-		->setTo($this->container->getParameter('admin_email'))
-		->setBody(
-				$this->renderView(
-						'MunicipalesBundle:Mail:sms_inbound_verification.html.twig',
-						array('phone' => $phone)
-				)
-		)
-		;
-		$this->get('mailer')->send($message);
+		// Look the phone and mail to verify
+		$entity_manager = $this->getDoctrine()->getManager();
+		$phone_verified_repository = $entity_manager->getRepository('Listabierta\Bundle\MunicipalesBundle\Entity\PhoneVerified');
+		 
+		$phone_verified = $phone_verified_repository->findOneBy(array('phone' => $phone, 'email' => $email));
+		
+		if(!empty($phone_verified) && $phone_verified->getTimestamp()->getTimestamp() <= 0)
+		{
+			$phone_verified->setTimestamp(new \DateTime());
+			
+			$entity_manager->persist($phone_verified);
+			$entity_manager->flush();
+			
+			$message = \Swift_Message::newInstance()
+			->setSubject('Telefono movil ' . $phone . ' verificado correctamente' . implode(',', $query))
+			->setFrom('verificaciones@municipales2015.listabierta.org', 'Verificaciones')
+			->setTo($email)
+			->setBody(
+					$this->renderView(
+							'MunicipalesBundle:Mail:sms_inbound_verification.html.twig',
+							array('phone' => $phone)
+					)
+			)
+			;
+			$this->get('mailer')->send($message);
+		}
+		else 
+		{
+			echo 'No phone found or already verified';
+		}
 		
 		return new Response('OK', 200);
 	}
