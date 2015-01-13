d51d3e1b7b4d953ee743f0de57fafe09496b58fc
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-01-13 17:16:07 +0100

    Update fields form for step 2

--- src/Listabierta/Bundle/MunicipalesBundle/Form/Type/CandidacyStep2Type.php
+++ src/Listabierta/Bundle/MunicipalesBundle/Form/Type/CandidacyStep2Type.php
@@ -5,12 +5,54 @@ namespace Listabierta\Bundle\MunicipalesBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class CandidacyStep2Type extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('program', 'file', array('required' => true))
+        $builder->add('program', 'file', array(
+        					'required' => true, 
+        					'constraints' => array(
+        						new Assert\NotBlank(),
+        					)
+        				)
+        			)
+        		->add('legal_conditions', 'file', array(
+        					'required' => true,
+        					'constraints' => array(
+        							new Assert\NotBlank(),
+        					)
+        				)
+        			)
+        		->add('recall_term', 'file', array(
+        					'required' => true,
+        					'constraints' => array(
+        							new Assert\NotBlank(),
+        					)
+        				)
+        			)
+        		->add('participatory_term', 'file', array(
+        					'required' => true,
+        					'constraints' => array(
+        							new Assert\NotBlank(),
+        					)
+        				)
+        			)
+        		->add('voter_conditions', 'file', array(
+        					'required' => true,
+        					'constraints' => array(
+        							new Assert\NotBlank(),
+        					)
+        				)
+        			)
+        		->add('technical_constrains', 'file', array(
+        					'required' => true,
+        					'constraints' => array(
+        							new Assert\NotBlank(),
+        					)
+        				)
+        			)
 	            ->add('continue', 'submit', array('attr' => array('class' => 'submit')));
     }
 	
