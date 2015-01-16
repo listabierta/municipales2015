f01d14580dfbabe594b4f99d6b09e9ece611b66d
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-01-16 02:09:07 +0100

    Add slugify bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Listabierta\Bundle\MunicipalesBundle\MunicipalesBundle(),
+        	new Cocur\Slugify\Bridge\Symfony\CocurSlugifyBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
