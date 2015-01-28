93e014d92da64a8f93be4915bcf8b6bc9a42521c
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-01-28 18:08:41 +0100

    Add academic level and language questions to step 3 form

--- src/Listabierta/Bundle/MunicipalesBundle/Form/Type/CandidateStep3Type.php
+++ src/Listabierta/Bundle/MunicipalesBundle/Form/Type/CandidateStep3Type.php
@@ -11,7 +11,31 @@ class CandidateStep3Type extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
+        $builder->add('academic_level', 'choice', array(
+			     	'choices' => array(
+			     			'1' => 'Sin estudios',
+			     			'2' => 'Estudios primarios o equivalentes', 
+			     			'3' => 'Enseñanza general secundaria, 1er ciclo',
+			     			'4' => 'Enseñanza Profesional de 2º grado, 2º ciclo',
+			     			'5' => 'Enseñanza general secundaria, 2º ciclo',
+			     			'6' => 'Enseñanzas profesionales superiores',
+			     			'7' => 'Estudios universitarios o equivalentes',
+			     			'8' => 'Máster universitario o doctorado',
+			     	),
+			     ))
+			     ->add('languages', 'choice', array(
+			     		'multiple' => TRUE,
+			     		'expanded' => TRUE,
+			     		'choices' => array(
+			     				'1' => 'Inglés',
+			     				'2' => 'Francés',
+			     				'3' => 'Alemán',
+			     				'4' => 'Italiano',
+			     				'5' => 'Chino',
+			     				'6' => 'Árabe',
+			     				'7' => 'Otro',
+			     		),
+			     ))
 	            ->add('continue', 'submit', array('attr' => array('class' => 'submit')));
     }
 	
