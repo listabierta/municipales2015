d8ea55aca40748c779f62b7ba82b0cfb6250b0a8
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-02-10 08:35:53 +0100

    Add new fields for town candidacy

--- src/Listabierta/Bundle/MunicipalesBundle/Form/Type/TownStep1Type.php
+++ src/Listabierta/Bundle/MunicipalesBundle/Form/Type/TownStep1Type.php
@@ -5,19 +5,81 @@ namespace Listabierta\Bundle\MunicipalesBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Listabierta\Bundle\MunicipalesBundle\Validator\Constraints\DNI;
 
 class TownStep1Type extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name', 'text', array('required' => true))
-        	    ->add('lastname', 'text', array('required' => true))
-        	    ->add('dni', 'text', array('required' => true))
-        	    ->add('email', 'email', array('required' => true))
-        	    ->add('province', 'text', array('required' => true))
-        	    ->add('town', 'text', array('required' => true))
-        	    ->add('phone', 'text', array('required' => true))
-	            ->add('continue', 'submit', array('attr' => array('class' => 'submit')));
+		 $builder->add('name', 'text', array(
+				'required' => true, 
+				'constraints' => array(
+					new Assert\NotBlank(),
+					new Assert\Length(array(
+							'min'        => 2,
+							'max'        => 255,
+							'minMessage' => 'Your first name must be at least {{ limit }} characters long',
+							'maxMessage' => 'Your first name cannot be longer than {{ limit }} characters long',
+					))
+				)))
+		->add('lastname', 'text', array(
+				'required' => true, 
+				'constraints' => array(
+					new Assert\NotBlank(),
+					new Assert\Length(array(
+							'min'        => 2,
+							'max'        => 255,
+							'minMessage' => 'Your lastname must be at least {{ limit }} characters long',
+							'maxMessage' => 'Your lastname cannot be longer than {{ limit }} characters long',
+					))
+				)))
+		->add('dni', 'text', array(
+				'required' => true, 
+				'constraints' => array(
+					new Assert\NotBlank(),
+					new DNI(),
+				)))
+		->add('username', 'text', array(
+				'required' => true,
+				'constraints' => array(
+						new Assert\NotBlank(),
+						new Assert\Length(array(
+								'min'        => 2,
+								'max'        => 255,
+								'minMessage' => 'Your username must be at least {{ limit }} characters long',
+								'maxMessage' => 'Your username cannot be longer than {{ limit }} characters long',
+						))
+				)))
+		->add('password', 'password', array(
+				'required' => true,
+				'constraints' => array(
+						new Assert\NotBlank(),
+						new Assert\Length(array(
+								'min'        => 8,
+								'max'        => 255,
+								'minMessage' => 'Your password must be at least {{ limit }} characters long',
+								'maxMessage' => 'Your password cannot be longer than {{ limit }} characters long',
+						))
+				)))
+		->add('email', 'email', array(
+				'required' => true, 
+				'constraints' => array(
+					new Assert\NotBlank(),
+					new Assert\Email(),
+				)))
+		->add('phone', 'text', array(
+				'required' => true, 
+				'constraints' => array(
+					new Assert\NotBlank(),
+					new Assert\Length(array(
+						'min'        => 9,
+						'max'        => 12,
+						'minMessage' => 'Your phone must be at least {{ limit }} characters long',
+						'maxMessage' => 'Your phone cannot be longer than {{ limit }} characters long',
+						))
+				)))
+		->add('continue', 'submit');
     }
 	
     public function setDefaultOptions(OptionsResolverInterface $resolver)
