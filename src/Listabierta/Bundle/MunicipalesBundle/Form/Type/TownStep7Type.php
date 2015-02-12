c88680bfd1f9fbe0567c197f776eabf0beb8b4ec
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-02-12 23:40:03 +0100

    Implement town step 7 for voters

---
+++ src/Listabierta/Bundle/MunicipalesBundle/Form/Type/TownStep7Type.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Listabierta\Bundle\MunicipalesBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Listabierta\Bundle\MunicipalesBundle\Validator\Constraints\DNI;
+
+class TownStep7Type extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('continue', 'submit', array('attr' => array('class' => 'submit')));
+    }
+	
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+    }
+
+    public function getName()
+    {
+        return 'town_step_7';
+    }
+}
