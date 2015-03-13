3a67965a966e6c021622531bffc65cd5278c449b
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-03-13 09:48:56 +0100

    Add new town step 7 confirmation type

---
+++ src/Listabierta/Bundle/MunicipalesBundle/Form/Type/TownStep7ConfirmationType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Listabierta\Bundle\MunicipalesBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Listabierta\Bundle\MunicipalesBundle\Validator\Constraints\DNI;
+
+class TownStep7ConfirmationType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('continue', 'submit', array('attr' => array('class' => 'submit')));
+    }
+	
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+    	$resolver->setDefaults(
+    			array(
+    					'allow_extra_fields' => TRUE
+    			)
+    	);
+    }
+
+    public function getName()
+    {
+        return 'town_step_7_confirmation';
+    }
+}
