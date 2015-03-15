d1cfdf0a55960d7cc0687f2afa7f6df836760c22
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-03-15 15:06:54 +0100

    Implement Province entity

---
+++ src/Listabierta/Bundle/MunicipalesBundle/Entity/Province.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Listabierta\Bundle\MunicipalesBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Province
+ */
+class Province
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Province
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
