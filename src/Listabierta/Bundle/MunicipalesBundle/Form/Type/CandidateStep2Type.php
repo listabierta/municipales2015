f861441376b1267ca5acb3debd377041f5be9089
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-03-17 19:10:46 +0100

    Load upload files conditionally based in files loaded via admin for form type in step 2 candidate

--- src/Listabierta/Bundle/MunicipalesBundle/Form/Type/CandidateStep2Type.php
+++ src/Listabierta/Bundle/MunicipalesBundle/Form/Type/CandidateStep2Type.php
@@ -9,57 +9,87 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class CandidateStep2Type extends AbstractType
 {
+	private static $loaded_files = array();
+	
+	public function __construct($loaded_files = array())
+	{
+		self::$loaded_files = $loaded_files;
+	}
+	
 	public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('program', 'file', array(
+    	if(self::$loaded_files['program'])
+    	{
+        	$builder->add('program', 'file', array(
         					'required' => FALSE, 
         					'data_class' => NULL,
         					'constraints' => array(
         						new Assert\File(array('maxSize' =>'1024k')),
         					)
         				)
-        			)
-        		->add('legal_conditions', 'file', array(
+        			);
+    	}
+    	
+    	if(self::$loaded_files['legal_conditions'])
+    	{
+	        $builder->add('legal_conditions', 'file', array(
+	        					'required' => FALSE,
+	        					'data_class' => NULL,
+	        					'constraints' => array(
+	        						new Assert\File(array('maxSize' =>'1024k')),
+	        					)
+	        				)
+	        			);
+    	}
+    	
+    	if(self::$loaded_files['recall_term'])
+    	{
+        	$builder->add('recall_term', 'file', array(
         					'required' => FALSE,
         					'data_class' => NULL,
         					'constraints' => array(
         						new Assert\File(array('maxSize' =>'1024k')),
         					)
         				)
-        			)
-        		->add('recall_term', 'file', array(
+        			);
+    	}
+    	
+    	if(self::$loaded_files['participatory_term'])
+    	{
+        	$builder->add('participatory_term', 'file', array(
         					'required' => FALSE,
         					'data_class' => NULL,
         					'constraints' => array(
         						new Assert\File(array('maxSize' =>'1024k')),
         					)
         				)
-        			)
-        		->add('participatory_term', 'file', array(
-        					'required' => FALSE,
-        					'data_class' => NULL,
-        					'constraints' => array(
-        						new Assert\File(array('maxSize' =>'1024k')),
-        					)
-        				)
-        			)
-        		->add('voter_conditions', 'file', array(
-        					'required' => FALSE,
-        					'data_class' => NULL,
-        					'constraints' => array(
-        						new Assert\File(array('maxSize' =>'1024k')),
-        					)
-        				)
-        			)
-        		->add('technical_constrains', 'file', array(
-        					'required' => FALSE,
-        				    'data_class' => NULL,
-        					'constraints' => array(
-        						new Assert\File(array('maxSize' =>'1024k')),
-        					)
-        				)
-        			)
-	            ->add('continue', 'submit', array('attr' => array('class' => 'submit')));
+        			);
+    	}
+    	
+    	if(self::$loaded_files['voter_conditions'])
+    	{
+	        $builder->add('voter_conditions', 'file', array(
+	        					'required' => FALSE,
+	        					'data_class' => NULL,
+	        					'constraints' => array(
+	        						new Assert\File(array('maxSize' =>'1024k')),
+	        					)
+	        				)
+	        			);
+    	}
+    	
+    	if(self::$loaded_files['technical_constrains'])
+    	{
+	        $builder->add('technical_constrains', 'file', array(
+	        					'required' => FALSE,
+	        				    'data_class' => NULL,
+	        					'constraints' => array(
+	        						new Assert\File(array('maxSize' =>'1024k')),
+	        					)
+	        				)
+	        			);
+    	}
+	    $builder->add('continue', 'submit', array('attr' => array('class' => 'submit')));
     }
 	
     public function setDefaultOptions(OptionsResolverInterface $resolver)
