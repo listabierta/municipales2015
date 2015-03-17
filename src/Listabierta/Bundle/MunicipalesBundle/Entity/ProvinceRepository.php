2f124cf841114cdd364a3c9d3d7c714b741d1ac0
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-03-17 18:07:03 +0100

    Fix result as array parsing

--- src/Listabierta/Bundle/MunicipalesBundle/Entity/ProvinceRepository.php
+++ src/Listabierta/Bundle/MunicipalesBundle/Entity/ProvinceRepository.php
@@ -24,13 +24,17 @@ class ProvinceRepository extends EntityRepository
 	public function getMunicipalityName($municipality_id)
 	{
 		$query = "SELECT name FROM municipalities_spain WHERE id='" . intval($municipality_id) . "' LIMIT 1";
-	
+
 		$statement = $this->getEntityManager()->getConnection()->executeQuery($query);
 		
 		$result = $statement->fetchAll();
 		
 		if(!empty($result))
 		{
+			if(!isset($result['name']))
+			{
+				$result = reset($result);
+			}
 			return $result['name'];
 		}
 		
