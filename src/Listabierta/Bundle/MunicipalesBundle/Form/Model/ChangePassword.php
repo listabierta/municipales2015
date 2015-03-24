c1ddb33943690051fb4723538935083dc2dc8a3d
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-03-24 05:51:45 +0100

    Add model form type for change password

---
+++ src/Listabierta/Bundle/MunicipalesBundle/Form/Model/ChangePassword.php
@@ -0,0 +1,23 @@
+<?php 
+namespace Listabierta\Bundle\MunicipalesBundle\Form\Model;
+
+use Symfony\Component\Security\Core\Validator\Constraints as SecurityAssert;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ChangePassword
+{
+    /**
+     * @SecurityAssert\UserPassword(
+     *     message = "Wrong value for your current password"
+     * )
+     */
+     protected $oldPassword;
+
+    /**
+     * @Assert\Length(
+     *     min = 6,
+     *     minMessage = "Password should by at least 6 chars long"
+     * )
+     */
+     protected $newPassword;
+}
