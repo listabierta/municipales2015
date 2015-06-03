3661ebdeb94364aac4b9264f35b861a16082baa7
Ángel Guzmán Maeso <shakaran@gmail.com>
2015-06-03 12:50:51 +0200

    Update form type for step conditions

---
+++ src/Listabierta/Bundle/MunicipalesBundle/Form/Type/Census/CensusStepConditionsType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Listabierta\Bundle\MunicipalesBundle\Form\Type\Census;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Listabierta\Bundle\MunicipalesBundle\Validator\Constraints\DNI;
+
+class CensusStepConditionsType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('conditions', 'choice', array(
+				        'choices' => array('yes' => 'SI', 'no' => 'NO'),
+        				'preferred_choices' => array('no'),
+        				'data' => 'no',
+				        'multiple' => false,
+				        'expanded' => true,
+				        'required' => true,
+				    ))
+        		->add('continue', 'submit', array('attr' => array('class' => 'submit','style' => 'margin-left:0px')));
+    }
+	
+    // 		<span>{{ form_widget(form.conditions, 'candidacy.step_conditions.accept'|trans) }}</span>
+	//	<span>{{ form_errors(form.conditions) }}</span>
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+    }
+
+    public function getName()
+    {
+        return 'census_step_conditions';
+    }
+}
